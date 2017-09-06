055e72950766f1fa63c4005227061fd2b91808df
Sarah Schaller <sarcsch@aol.com>
2017-09-06 00:07:22 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,27 +1,27 @@
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 1, 1)
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("Lord of the Rings", 2, 1)
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 1, 1);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Lord of the Rings", 2, 1);
 
-INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Sorcerer's Stone", 1997, 1)
-INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Chamber of Secrets", 1998, 1)
-INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Prisoner of Azkaban", 1999, 1)
-INSERT INTO books (title, year, series_id) VALUES ("The Fellowship of the Ring", 1954, 2)
-INSERT INTO books (title, year, series_id) VALUES ("The Two Towers", 1954, 2)
-INSERT INTO books (title, year, series_id) VALUES ("The Return of the King", 1955, 2)
+INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Sorcerer's Stone", 1997, 1);
+INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Chamber of Secrets", 1998, 1);
+INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Prisoner of Azkaban", 1999, 1);
+INSERT INTO books (title, year, series_id) VALUES ("The Fellowship of the Ring", 1954, 2);
+INSERT INTO books (title, year, series_id) VALUES ("The Two Towers", 1954, 2);
+INSERT INTO books (title, year, series_id) VALUES ("The Return of the King", 1955, 2);
 
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Harry Potter", "What?", "Wizard", 1, 1)
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Ronald Weasley", "Wow!", "Wizard", 1, 1)
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Hagrid", "You're a wizard Harry!", "Half-giant", 1, 1)
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Sirius Black", "I believe in you Harry", "Wizard", 1, 1)
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Frodo Baggins", "I'm glad to be with you here at the end of all things", "Hobbit", 2, 2)
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Bilbo Baggins", "Eleventy-first", "Hobbit", 2, 2)
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Gollum", "My precious", "River Creature", 2, 2)
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Gandalf", "I am Gandalf the White", "Wizard", 2, 2)
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Harry Potter", "What?", "Wizard", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Ronald Weasley", "Wow!", "Wizard", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Hagrid", "You're a wizard Harry!", "Half-giant", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Sirius Black", "I believe in you Harry", "Wizard", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Frodo Baggins", "I'm glad to be with you here at the end of all things", "Hobbit", 2, 2);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Bilbo Baggins", "Eleventy-first", "Hobbit", 2, 2);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Gollum", "My precious", "River Creature", 2, 2);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Gandalf", "I am Gandalf the White", "Wizard", 2, 2);
 
-INSERT INTO subgenres (name) VALUES ("Fantasy")
-INSERT INTO subgenres (name) VALUES ("Historical Fiction")
+INSERT INTO subgenres (name) VALUES ("Fantasy");
+INSERT INTO subgenres (name) VALUES ("Historical Fiction");
 
-INSERT INTO authors (name) VALUES ("J.K. Rowling")
-INSERT INTO authors (name) VALUES ("J.R.R. Tolkien")
+INSERT INTO authors (name) VALUES ("J.K. Rowling");
+INSERT INTO authors (name) VALUES ("J.R.R. Tolkien");
 --
 -- INSERT INTO character_books (book_id, character_id) VALUES ()
 -- INSERT INTO character_books (book_id, character_id) VALUES ()
